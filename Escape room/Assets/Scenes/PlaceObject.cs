b8b83e5af05c8b492a8322b2bdde88d28923f5ed
lukekoko <lukekoko1@gmail.com>
2020-08-30 21:38:57 +1000

    Changes - Created prototype scripts for:     - placing objects on a plane with touch     - Spawning an object on a horizontal plane on startup - Downgraded ARFoundation version to the Unity recommended version as the latest I was having errors running on my phone

---
+++ Escape room/Assets/Scenes/PlaceObject.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class PlaceObject : MonoBehaviour
+{
+    public GameObject objectToSpawn;
+    private Indicator indicator;
+    private ARRaycastManager rayManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        indicator = FindObjectOfType<Indicator>();
+        rayManager = FindObjectOfType<ARRaycastManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // if (Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began)
+        // {
+        //     GameObject obj = Instantiate(objectToSpawn, indicator.transform.position, indicator.transform.rotation);
+        // }
+        List<ARRaycastHit> hits = new List<ARRaycastHit>();
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Began)
+            {
+                if (rayManager.Raycast(touch.position, hits, TrackableType.PlaneWithinBounds))
+                {
+                    GameObject obj = Instantiate(objectToSpawn, hits[0].pose.position, hits[0].pose.rotation);
+                }
+            }
+        }
+
+    }
+}
