4287854362a8187fe733558c12cacc5064404057
lukekoko <lukekoko1@gmail.com>
2020-09-06 22:25:51 +1000

    Add scene that calculates a basic area of detected planes

---
+++ Escape room/Assets/PlaneAreaBehaviour.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.XR.ARFoundation;
+
+public class PlaneAreaBehaviour : MonoBehaviour
+{
+    public TextMeshPro areaText;
+    public ARPlane ARPlane;
+
+    private void OnEnable()
+    {
+        ARPlane.boundaryChanged += ArPlaneBoundaryChanged;
+    }
+
+    private void OnDisable()
+    {
+        ARPlane.boundaryChanged -= ArPlaneBoundaryChanged;
+    }
+
+    private void ArPlaneBoundaryChanged(ARPlaneBoundaryChangedEventArgs obj)
+    {
+        areaText.text = CalculatePlaneArea(ARPlane).ToString();
+    }
+    private float CalculatePlaneArea(ARPlane plane)
+    {
+        return plane.size.x * plane.size.y;
+    }
+
+    public void ToggleAreaView()
+    {
+        if (areaText.enabled)
+            areaText.enabled = false;
+        else
+            areaText.enabled = true;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        areaText.transform.rotation = Quaternion.LookRotation(areaText.transform.position - Camera.main.transform.position);
+    }
+}
---
+++ Escape room/Assets/PlaneAreaManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class PlaneAreaManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            var touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Ended)
+            {
+                if (Input.touchCount == 1)
+                {
+                    Ray raycast = Camera.main.ScreenPointToRay(touch.position);
+                    if (Physics.Raycast(raycast, out RaycastHit raycastHit))
+                    {
+                        var planeAreaBehaviour = raycastHit.collider.gameObject.GetComponent<PlaneAreaBehaviour>();
+                        if (planeAreaBehaviour != null)
+                        {
+                            planeAreaBehaviour.ToggleAreaView();
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
