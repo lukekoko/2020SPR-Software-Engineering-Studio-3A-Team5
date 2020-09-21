0ba63c8aa73cf5ab9542d3ff777d79706471332e
Maral <37317245+MaralNalbandian@users.noreply.github.com>
2020-09-21 14:11:12 +1000

    Allow balloons to move up the screen

--- Escape room/Assets/Scripts/SpawnScript.cs
+++ Escape room/Assets/Scripts/SpawnScript.cs
@@ -10,12 +10,20 @@ public class SpawnScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(StartSpawning());
     }
 
-    // // Update is called once per frame
-    // void Update()
-    // {
-    //     
-    // }
+    IEnumerator StartSpawning()
+    {
+        yield return new WaitForSeconds(4);
+
+        for (int i = 0; i < 3; i++)
+        {
+            Instantiate(balloons[i], spawnPoints[i].position, Quaternion.identity);
+        }
+
+        StartCoroutine(StartSpawning());
+
+    }
+    
 }
