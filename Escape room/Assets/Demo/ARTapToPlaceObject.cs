d0ebc5982ae1d2cd2ff536bc85a539e41003352c
Jacob <jacob@Jacobs-MBP.modem>
2020-10-18 21:24:06 +1100

    WIP: Logic

--- Escape room/Assets/Demo/ARTapToPlaceObject.cs
+++ Escape room/Assets/Demo/ARTapToPlaceObject.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.ARFoundation;
@@ -9,12 +9,23 @@ using UnityEngine.XR.ARSubsystems;
 public class ARTapToPlaceObject : MonoBehaviour
 {
     public GameObject placementIndicator;
-    public GameObject objectToPlace;
+    public GameObject stakeObject;
+    public GameObject hoopObject;
 
     private ARSessionOrigin arOrigin;
     private ARRaycastManager arManager;
     private Pose placementPose;
     private bool placementPoseIsValid = false;
+    private bool isStakePlaced = false;
+    private int hoopObjectPlacedCap = 3;
+    private List<GameObject> hoopObjectsPlaced = new List<GameObject>();
+    
+    private Vector2 startPos;
+    private Vector2 endPos;
+    private float startTime;
+    private float diffTime;
+    private float distance;
+    private float speed;
 
     void Start()
     {
@@ -31,16 +42,48 @@ public class ARTapToPlaceObject : MonoBehaviour
 
         UpdatePlacementPose();
         UpdatePlacementIndicator();
-
-        if (placementPoseIsValid && Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase > 0)
         {
-            PlaceObject();
+            Touch touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Began)
+            {
+                startPos = touch.position;
+                startTime = Time.time;
+                if (!isStakePlaced && placementPoseIsValid) {
+                    PlaceStake();
+                }
+            } else if (touch.phase == TouchPhase.Ended) {
+                endPos = touch.position;
+                diffTime = Time.time - startTime;
+                startTime = 0;
+                distance = Vector2.Distance(startPos,endPos);
+                if(diffTime != 0)
+                {
+                    speed = distance / diffTime;
+                }
+                if (speed != 0 && isStakePlaced && hoopObjectsPlaced.Count < hoopObjectPlacedCap)
+                {
+                    ThrowHoop(speed);
+                }
+                // logic to detect swipe movement and instantiate hoopObject in front of camera with velocity of throwing related to speed of swipe of the finger
+                // increment the amount of objects placed
+                // Once cap is reached, go to next if statement
+            }
         }
     }
 
-    private void PlaceObject()
+    private void PlaceStake()
     {
-        Instantiate(objectToPlace, placementPose.position, placementPose.rotation);
+        Instantiate(stakeObject, placementPose.position, placementPose.rotation);
+        isStakePlaced = true;
+    }
+    
+    private void ThrowHoop(float speed)
+    {
+        GameObject hoop = Instantiate(hoopObject, Camera.current.transform.position, new Quaternion(0, 0, 0, 1));
+        hoop.GetComponent<Rigidbody>().AddForce(new Vector3(0, speed/20*(distance/diffTime), 2));
+        speed = 0;
+        hoopObjectsPlaced.Add(hoop);
     }
 
     private void UpdatePlacementIndicator()
